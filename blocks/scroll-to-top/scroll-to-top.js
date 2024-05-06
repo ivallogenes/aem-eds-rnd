770acb7c6a022ccbcace3e8ba8f7d173ee10d2b5
Dobreva Iveta <ateviel@gmail.com>
2024-05-06 14:44:08 +0000

    Add scroll to top block css and js.

---
+++ blocks/scroll-to-top/scroll-to-top.js
@@ -0,0 +1,31 @@
+// we need to setInterval otherwise scrollTopBtn is initially null
+const interval = setInterval(() => {
+  const scrollTopBtn = document.querySelector('[data-block-name="scroll-to-top"]');
+
+  if (scrollTopBtn) {
+    scrollTopAdd();
+    clearInterval(interval);
+  }
+}, 1000);
+
+
+const scrollTopAdd = () => {
+  const isDesktop = window.matchMedia('(min-width: 640px)');
+  const scrollTopBtn = document.querySelector('[data-block-name="scroll-to-top"]');
+
+  if (isDesktop.matches && scrollTopBtn) {
+
+    scrollTopBtn.addEventListener('click', function() {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    });
+
+    window.addEventListener('scroll', function() {
+      if (window.scrollY > 600) {
+        scrollTopBtn.style.display = 'block';
+      } else {
+        scrollTopBtn.style.display = 'none';
+      }
+    });
+
+  }
+}
